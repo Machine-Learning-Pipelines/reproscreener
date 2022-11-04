465f43210d17f936ecadc0630a2d7f47403728ca
adb16x <adhithyabhaskar@gmail.com>
2022-11-04 15:58:12 -0700

    Working score list splitting

--- src/find_regex.py
+++ src/find_regex.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from pandas import read_csv, concat
+from pandas import DataFrame, read_csv, concat
 import keywords
 
 def init_environment(num_articles=50, folder_name_base='mine', base_dir='./case-studies/arxiv-corpus/', compare_manual=False):
@@ -11,7 +11,7 @@ def init_environment(num_articles=50, folder_name_base='mine', base_dir='./case-
     gunderson_vars = ["problem", "objective", "research_method", "research_questions", "pseudocode", "training_data", "validation_data", "test_data", "results", "hypothesis", "prediction", "method_source_code", "hardware_specifications", "software_dependencies", "experiment_setup", "experiment_source_code", "affiliation"]
     
     repro_eval = init_repro_eval(path_corpus, num_articles, gunderson_vars)
-    found_vars = calculate_repro_eval_scores(path_corpus, repro_eval)[['id', 'found_vars', 'affiliation']]
+    found_vars = calculate_repro_eval_scores(path_corpus, repro_eval)
     repro_eval_filled = set_repro_eval_scores(concat([repro_eval, found_vars],
                                                      axis=0,
                                                      join='inner'), gunderson_vars)
@@ -19,6 +19,7 @@ def init_environment(num_articles=50, folder_name_base='mine', base_dir='./case-
 
 def init_repro_eval(path_corpus, num_articles, variables):
     # Create an empty repro_eval dataframe with provided @variables
+    # TODO Find where repro_eval's empty columns are getting removed before print/return
     repro_eval = read_csv(path_corpus + 'scrape_df_' + 
                           str(num_articles) + '.csv', dtype=object)
     repro_eval = repro_eval[['id']]
@@ -32,17 +33,21 @@ def calculate_repro_eval_scores(path_corpus, df):
     df['title'] = df['soup'].apply(lambda x: x.title.getText())
     print("Finding variables in files...")
     df['found_vars'] = df['soup'].apply(lambda x: keywords.find_vars(x))
-    df['affiliation'] = df['soup'].apply(lambda x: keywords.find_affiliation(x))
-    return df[['id', 'title', 'found_vars', 'affiliation']]
+    df['affiliation_calc'] = df['soup'].apply(lambda x: keywords.find_affiliation(x))
+    return df[['id', 'title', 'found_vars', 'affiliation_calc']]
+
+def calc_found_vars_array(found_vars, variables):
+    vars_array = [0] * len(variables)
+    for i, var in enumerate(variables):
+        vars_array[i] = 1 if var in found_vars else 0
+    return vars_array
 
 def set_repro_eval_scores(df, variables):
-    for col in df.columns.drop(['id', 'affiliation', 'found_vars']):
-        df['found_vars_array'] = df['found_vars'].apply(lambda x: calc_found_vars_array(x, variables.remove('id')))
-    return df
-        # if col in df['found_vars']:
-        #     repro_eval.at[index, col] = 1
-        # else:
-        #     repro_eval.at[index, col] = 0
+    # for col in df.columns.drop(['id', 'affiliation', 'found_vars']):
+    df['found_vars_array'] = df['found_vars'].apply(lambda x: calc_found_vars_array(x, variables))
+    scores = DataFrame(df.found_vars_array.to_list(), index = df.index, columns=variables)
+    scores['affiliation'] = df['affiliation_calc']
+    return scores
 
 
 # test1 = {'pseudocode', 'objective', 'research_questions'}
@@ -50,11 +55,7 @@ def set_repro_eval_scores(df, variables):
 
 init_environment()
 
-def calc_found_vars_array(found_vars, variables):
-    vars_array = [0] * len(variables)
-    for i, var in enumerate(variables):
-        vars_array[i] = 1 if var in found_vars else 0
-    return vars_array
+
 
 # calc_found_vars_array(test1, gunderson_vars.remove('id'))
 # init_environment()
