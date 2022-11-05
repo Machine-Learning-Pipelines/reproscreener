bd24996519bd204f902c1ba8f46ff34e73d8e195
adb16x <adhithyabhaskar@gmail.com>
2022-11-04 17:23:57 -0700

    find_regex -> evaluate_guidance, add tabulate

--- src/main.py
+++ src/main.py
@@ -16,7 +16,7 @@ def run_reproscreener():
     # print(output_repro_eval)
     # print(eg.get_manual_eval(path_corpus))
     output_repro_eval.to_csv(path_corpus + 'output/repro_eval.csv', index_label="index")
-    manual_df = eg.get_manual(path_corpus)
+    manual_df = eg.get_manual_eval(path_corpus)
     eg.compare_available_manual(output_repro_eval, manual_df, gunderson_vars)
     
     return(output_repro_eval)
