f7d8c9e1d231688b262c9b5ed8972ca792d15637
Adhithya Bhaskar <adhithyabhaskar@gmail.com>
2022-12-17 15:12:54 -0800

    working progress bars, scrape_arxiv

--- src/main.py
+++ src/main.py
@@ -2,7 +2,9 @@ import evaluate_guidance as eg
 from pandas import concat, merge
 import streamlit as st
 import read_tex
+
 st.set_page_config(layout="wide")
+
 
 def run_reproscreener():
     """_summary_
@@ -27,30 +29,29 @@ def run_reproscreener():
         "experiment_source_code",
         "affiliation",
     ]
-    num_articles=50
-    path_corpus = eg.init_paths(num_articles = 50, folder_name="mine50-csLG/")
+    num_articles = 50
+    path_corpus = eg.init_paths(num_articles=50, folder_name="mine50-csLG/")
     repro_eval = eg.init_repro_eval(path_corpus, num_articles)
-    
+
     found_vars_pdf = eg.get_found_vars(path_corpus, repro_eval)
     found_vars_tex = read_tex.get_found_vars_tex(path_corpus, repro_eval)
 
     repro_eval_filled_pdf = eg.set_repro_eval_scores(
-        concat([repro_eval, found_vars_pdf], axis=0, join="inner"),
-        gunderson_vars
+        concat([repro_eval, found_vars_pdf], axis=0, join="inner"), gunderson_vars
     )
     repro_eval_filled_tex = eg.set_repro_eval_scores(
         concat([repro_eval, found_vars_tex], axis=0, join="inner"),
         gunderson_vars,
-        skip_affiliation=True
+        skip_affiliation=True,
     )
-    
+
     output_repro_eval_pdf = merge(
         found_vars_pdf[["id", "title"]],
         repro_eval_filled_pdf,
         left_index=True,
         right_index=True,
     ).drop_duplicates(subset=["id"])
-    
+
     output_repro_eval_tex = merge(
         found_vars_tex[["id", "title"]],
         repro_eval_filled_tex,
@@ -60,12 +61,22 @@ def run_reproscreener():
 
     # print(output_repro_eval_tex)
     # print(eg.get_manual_eval(path_corpus))
-    output_repro_eval_pdf.to_csv(path_corpus + "output/repro_eval.csv", index_label="index")
-    output_repro_eval_tex.to_csv(path_corpus + "output/repro_eval_tex.csv", index_label="index")
-    
-    eg.compare_available_manual(output_repro_eval_pdf, output_repro_eval_tex, eg.get_manual_eval(path_corpus), gunderson_vars)
+    output_repro_eval_pdf.to_csv(
+        path_corpus + "output/repro_eval.csv", index_label="index"
+    )
+    output_repro_eval_tex.to_csv(
+        path_corpus + "output/repro_eval_tex.csv", index_label="index"
+    )
+
+    eg.compare_available_manual(
+        output_repro_eval_pdf,
+        output_repro_eval_tex,
+        eg.get_manual_eval(path_corpus),
+        gunderson_vars,
+    )
     # eg.compare_available_manual(output_repro_eval_tex, eg.get_manual_eval(path_corpus), gunderson_vars)
 
     return output_repro_eval_pdf
+
 
 st.dataframe(run_reproscreener(), use_container_width=True)
