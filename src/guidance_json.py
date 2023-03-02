c3dc4bbd2b954ec37fa6eac02cb68b765bb2831c
Adhithya Bhaskar <adhithyabhaskar@gmail.com>
2023-03-02 09:20:52 -0800

    auto gdrive csv download working

--- src/guidance_json.py
+++
@@ -1,8 +0,0 @@
-import json
-import pandas as pd
-import streamlit as st
-from pathlib import Path
-
-with open(Path("guidance/gunderson-2018-revised/main-codebook.json"), "r") as f:
-    df_json = pd.json_normalize(json.load(f), record_path=["variables"])
-    st.write(df_json)
