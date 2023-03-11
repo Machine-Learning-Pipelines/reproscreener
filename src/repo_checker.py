12a351e666a7265635dec116d18e59df72a4bc28
Adhithya Bhaskar <adhithyabhaskar@gmail.com>
2023-03-11 08:59:28 -0800

    repo checker

---
+++ src/repo_checker.py
@@ -0,0 +1,121 @@
+from pathlib import Path
+from console import console
+from markdown_it import MarkdownIt
+from bs4 import BeautifulSoup
+import re
+import glob
+
+
+def only_found_dict_vals(d, only_found):
+    if only_found:
+        out = {k: v for k, v in d.items() if v}
+    else:
+        out = d
+    if out:
+        return out
+    return None
+
+
+def get_downloaded_repos(path_corpus):
+    path_repos = path_corpus / "repo"
+    downloaded_repos = [p.name for p in path_repos.iterdir() if p.is_dir()]
+    return downloaded_repos
+
+
+def check_dependencies(path_corpus, paper, only_found=False):
+    path_paper = path_corpus / "repo" / paper
+    file_names = [
+        "Dockerfile",
+        "requirements.txt",
+        "setup.py",
+        "environment.yaml",
+        "Pipfile",
+        "Pipfile.lock",
+        "pyproject.toml",
+        "pip_reqs.txt",
+        "conda_reqs.txt",
+    ]
+    paths_dependencies = [path_paper / file_name for file_name in file_names]
+    exists = {
+        file_name: Path(path_paper / file_name).is_file() for file_name in file_names
+    }
+    return only_found_dict_vals(exists, only_found)
+
+
+def check_reproducibility_scripts(path_corpus, paper, only_found=False):
+    path_paper = path_corpus / "repo" / paper
+    file_names = ["plots.py", "run_experiments.py"]
+    file_paths = [path_paper / file_name for file_name in file_names]
+
+
+def parse_readme(path_corpus, paper, only_found=False):
+    path_readme = path_corpus / "repo" / paper / "README.md"
+    checks = ["requirements", "dependencies", "setup", "install"]
+    if Path(path_readme).is_file():
+        with open(path_readme, "r") as f:
+            readme = f.read()
+            md = MarkdownIt("gfm-like")
+            html = md.render(readme)
+            soup = BeautifulSoup(html, "lxml")
+            headings = soup.find_all(re.compile("^h[1-6]$"))
+            exists = {
+                check: any(check in heading.text.lower() for heading in headings)
+                for check in checks
+            }
+            return only_found_dict_vals(exists, only_found)
+    return None
+
+
+def check_wrapper_script(path_corpus, paper, only_found=False):
+    path_paper = path_corpus / "repo" / paper
+    file_names = [
+        "run",
+        "run.py",
+        "main.sh",
+        "main.py",
+        "run_all.sh",
+        "run_all.py",
+        "run_experiments.sh",
+        "run_experiments.py",
+        "MAKEFILE",
+        "Makefile",
+        "Dockerfile",
+    ]
+    # exists = {
+    #     file_name: Path(path_paper / file_name).is_file() for file_name in file_names
+    # }
+    # [file_names.index(file.stem) for file in path_paper.iterdir() if file.is_file()]
+
+    exists = {file_name: False for file_name in file_names}
+    for file in path_paper.iterdir():
+        if file.is_file():
+            try:
+                found_index = file_names.index(file.stem)
+            except ValueError:
+                found_index = -1
+                exists[file.stem] = False
+            if found_index != -1:
+                exists[file_names[found_index]] = True
+
+    return only_found_dict_vals(exists, only_found)
+
+
+def check_all_paper(path_corpus, paper, only_found=False):
+    checks = {
+        "wrapper_script": check_wrapper_script(
+            path_corpus, paper, only_found=only_found
+        ),
+        "dependencies": check_dependencies(path_corpus, paper, only_found=only_found),
+        "readme": parse_readme(path_corpus, paper, only_found=only_found),
+    }
+    return only_found_dict_vals(checks, only_found)
+
+
+if __name__ == "__main__":
+    path_corpus = Path("case-studies/arxiv-corpus/mine50-andor")
+    downloaded_repos = get_downloaded_repos(path_corpus)
+
+    for paper in downloaded_repos:
+        console.rule(f"Paper: {paper}", style="bold red")
+        console.print(check_all_paper(path_corpus, paper, only_found=True))
+        console.print()
