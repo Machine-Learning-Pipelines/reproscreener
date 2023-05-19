bf8c8497ac36b4c9099741e1f279a90dc716eac8
Adhithya Bhaskar <adhithyabhaskar@gmail.com>
2023-05-19 13:05:02 -0700

    Visual result update (tables), tests

--- tests/download_arxiv_test.py
+++ tests/download_arxiv_test.py
@@ -1,27 +1,27 @@
 import tarfile
 from pathlib import Path
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 
 from reproscreener.download_arxiv import download_extract_source
 
 
-def test_download_extract_source(mocker):
+@patch("requests.get")
+@patch("tarfile.open")
+def test_download_extract_source(mock_tarfile_open, mock_requests_get):
     arxiv_url = "https://arxiv.org/e-print/2106.07704"
     path_download = Path("tests/tmp/")
 
     # Mock requests.get()
-    mock_get_patcher = mocker.patch("requests.get")
     mock_response = Mock()
     mock_response.raw = Mock()
-    mock_get_patcher.return_value = mock_response
+    mock_requests_get.return_value = mock_response
 
     # Mock tarfile.open()
-    mock_tarfile = mocker.MagicMock()
-    mock_opened_tarfile = mocker.MagicMock()
-    mock_tarfile.__enter__.return_value = mock_opened_tarfile
-    mocker.patch("tarfile.open", return_value=mock_tarfile)
+    mock_opened_tarfile = Mock()
+    mock_tarfile_open.return_value.__enter__.return_value = mock_opened_tarfile
 
     result = download_extract_source(arxiv_url, path_download)
-    mock_get_patcher.assert_called_once_with(arxiv_url, stream=True, timeout=5)
+    mock_requests_get.assert_called_once_with(arxiv_url, stream=True, timeout=5)
+    mock_tarfile_open.assert_called_once_with(fileobj=mock_response.raw, mode="r|gz")
     mock_opened_tarfile.extractall.assert_called_once_with(path_download)
-    assert result == path_download
+    assert result[1] == path_download
