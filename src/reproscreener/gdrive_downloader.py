60e5aaecee4e3b7e695dda0ee9431e963c67e8f1
Adhithya Bhaskar <adhithyabhaskar@gmail.com>
2023-05-28 00:17:45 -0700

    working repo_eval heatmaps (regular & grouped)

---
+++ src/reproscreener/gdrive_downloader.py
@@ -0,0 +1,72 @@
+import pandas as pd
+from pathlib import Path
+import numpy as np
+
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
+
+from reproscreener.utils import console
+
+
+def gdrive_authenticate():
+    gauth = GoogleAuth()
+    gauth.LocalWebserverAuth()
+    drive = GoogleDrive(gauth)
+    return drive
+
+
+def gdrive_download_manual_eval(drive, manual_path):
+    file_list = drive.ListFile({"q": "'root' in parents and trashed=false"}).GetList()
+    for file1 in file_list:
+        if file1["title"] == "repro-screener":
+            file_list2 = drive.ListFile({"q": "'%s' in parents and trashed=false" % file1["id"]}).GetList()
+            for file2 in file_list2:
+                if file2["id"] == "12FpKYUWtM8zLREZkdZGYR0eRWxcsbJhHwMbS_yuE46s":
+                    file3 = drive.CreateFile({"id": file2["id"]})
+                    file3.GetContentFile(
+                        filename=manual_path,
+                        mimetype="text/csv",
+                    )
+                    console.print("Downloaded file: %s" % file2["title"])
+                    return manual_path
+
+
+def gdrive_get_manual_eval(overwrite=False, manual_path="case-studies/arxiv-corpus/manual_eval.csv"):
+    dtypes_manual = {
+        "paper": str,
+        "notes": str,
+        "empirical_dataset": str,
+        "article_link_avail": float,
+        "code_available_article_desc": str,
+        "code_avail_url": str,
+        "pwc_link_avail": float,
+        "pwc_link_match": float,
+        "pwc_link_desc": str,
+        "result_replication_code_avail": float,
+        "code_language": str,
+        "package": float,
+        "wrapper_script": str,
+        "hardware_specifications": str,
+        "software_dependencies": str,
+        "will_it_reproduce": float,
+    }
+    path_exists = Path(manual_path).is_file()
+
+    if path_exists and not overwrite:
+        console.print("Manual eval file already exists, use the overwrite flag to download")
+
+    if (not path_exists) or (path_exists and overwrite):
+        drive = gdrive_authenticate()
+        manual_path = gdrive_download_manual_eval(drive, manual_path)
+
+    manual_eval = pd.read_csv(manual_path)
+    manual_eval.replace("nan", np.nan)
+    manual_eval = manual_eval.drop(index=[0, 51])
+    manual_eval = manual_eval.astype(dtypes_manual)
+    return manual_eval
+
+
+if __name__ == "__main__":
+    manual_path = Path("case-studies/arxiv-corpus/manual_eval.csv")
+    manual = gdrive_get_manual_eval(overwrite=False, manual_path=manual_path)
+    console.print(manual)
--- src/reproscreener/utils.py
+++ src/reproscreener/utils.py
@@ -17,3 +17,43 @@ logging.basicConfig(
 )
 
 log = logging.getLogger("rich")
+
+ext_mapping = {
+    "Dependencies": {
+        "Dockerfile": [],
+        "requirements": [".txt"],
+        "setup": [".py"],
+        "environment": [".yml"],
+        "Pipfile": [],
+        "pyproject": [".toml"],
+        "pip_reqs": [".txt"],
+        "conda_reqs": [".txt"],
+    },
+    "Wrapper Scripts": {
+        "run": [".py", ".sh"],
+        "main": [".py", ".sh"],
+        "run_all": [".py", ".sh"],
+        "run_experiments": [".py", ".sh"],
+        "MAKEFILE": [],
+        "Makefile": [],
+        "Dockerfile": [],
+    },
+    "Parsed Readme": {
+        "readme_requirements": [""],
+        "readme_dependencies": [""],
+        "readme_setup": [""],
+        "readme_install": [""],
+    },
+    "Others": {
+        "No code provided": [""],
+        "Code provided but no matches": [""],
+    },
+}
+
+# Create the reverse mapping
+reverse_mapping = {
+    file + ext: category
+    for category, files in ext_mapping.items()
+    for file, extensions in files.items()
+    for ext in extensions
+}
