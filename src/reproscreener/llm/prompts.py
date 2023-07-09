c3dfc0a20f6e31e532c18dd33f6f62c23d7ca719
Adhithya Bhaskar <adhithyabhaskar@gmail.com>
2023-07-09 12:25:42 -0700

    spectre

--- src/reproscreener/llm/prompts.py
+++ src/reproscreener/llm/prompts.py
@@ -1,36 +1,37 @@
 import os
 import openai
 import pandas as pd
+import tiktoken
 from pathlib import Path
-from rich.table import Table
 from rich.progress import Progress
 from reproscreener.utils import log, console
+
+
+def num_tokens_from_string(string: str, encoding_name: str) -> int:
+    encoding = tiktoken.get_encoding(encoding_name)
+    num_tokens = len(encoding.encode(string))
+    return num_tokens
 
 
 class OpenAILLM:
     def __init__(self, api_key: str):
         openai.api_key = api_key
-        self.prompts = [
-            "What is the problem the research seeks to solve?",
-            "What is the objective of the research?",
-            "What is the research method used?",
-            "What are the research questions asked?",
-            "Is there any method described using pseudo code?",
-            "Is the datset made available or shared?",
-            "What are the hypotheses that the authors make prior to conducting the experiment?",
-            "What are the predicted results by the authors?",
-            "Is the code for the research open source and shared?",
-            "What are the software dependencies?",
-            "Is the experimental setup described?",
-        ]
+
+        prompt_path = Path(__file__).parent.resolve() / "prompt.txt"
+        with open(prompt_path, "r") as f:
+            first_half_prompt = f.read()
+        self.prompt = first_half_prompt + "Here is the abstract to check:\n'{}'"
+
         self.path = Path("case-studies/arxiv-corpus/gold_standard/abstracts")
         self.files = list(self.path.glob("*.txt"))
 
     def analyze_abstracts(
         self, max_papers: int = None, output_path: Path = Path("case-studies/arxiv-corpus/gold_standard/output")
     ) -> pd.DataFrame:
-        data = {"paper": [], "metric": [], "matched_text": [], "value": []}
-
+        data = {
+            "paper": [],
+            "response_text": [],
+        }
         files = self.files[:max_papers] if max_papers else self.files
 
         with Progress() as progress:
@@ -40,12 +41,10 @@ class OpenAILLM:
                 log.info(f"Processing paper: {file.stem}")
                 with open(file, "r") as f:
                     abstract = f.read().replace("\n", " ")
-                result = self._analyze_abstract(abstract, progress)
-                for metric, text in result.items():
-                    data["paper"].append(file.stem)
-                    data["metric"].append(metric)
-                    data["matched_text"].append(text)
-                    data["value"].append(1 if text else 0)
+                result = self._analyze_abstract(abstract)
+                data["paper"].append(file.stem)
+                data["response_text"].append(result["response_text"])
+
                 progress.advance(abstract_task)
 
         df = pd.DataFrame(data)
@@ -53,26 +52,23 @@ class OpenAILLM:
 
         return df
 
-    def _analyze_abstract(self, abstract: str, progress: Progress) -> dict:
-        results = {}
-        prompt_task = progress.add_task("[cyan]Processing prompts...", total=len(self.prompts))
-
-        for prompt in self.prompts:
-            log.info(f"Processing prompt: {prompt}")
-            response = openai.Completion.create(
-                engine="text-davinci-003",
-                prompt=f"This is an abstract from a research paper: '{abstract}'. {prompt}",
-                temperature=0.5,
-                max_tokens=150,
-            )
-            results[prompt] = response.choices[0].text.strip()
-            progress.advance(prompt_task)
-
-        progress.reset(prompt_task)
-        return results
+    def _analyze_abstract(self, abstract: str) -> dict:
+        log.info(f"Processing prompt")
+        prompt_tokens = num_tokens_from_string(self.prompt.format(abstract), "p50k_base")
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=self.prompt.format(abstract),
+            temperature=0.5,
+            max_tokens=4096 - len(self.prompt.format(abstract).split()) - 350,
+        )
+        console.print(response, style="bold cyan")
+        console.print(f"Tokens in prompt: {prompt_tokens}", style="bold red")
+        text = response.choices[0].text.strip()
+        return {"response_text": text}
 
 
 if __name__ == "__main__":
     openai_llm = OpenAILLM(os.getenv("OPENAI_API_KEY"))
-    df = openai_llm.analyze_abstracts(max_papers=1)
-    print(df)
+    df = openai_llm.analyze_abstracts(max_papers=2)
+    for index, row in df.iterrows():
+        console.print(f"Paper: {row['paper']}\nResponse Text:\n{row['response_text']}\n")
