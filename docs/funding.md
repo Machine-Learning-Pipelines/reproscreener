610f3bcdee2e8110fde70663de2f1a69a29623ec
Adhithya Bhaskar <adhithyabhaskar@gmail.com>
2023-08-01 22:41:23 -0700

    gold standard heatmaps, funding, eval tables

--- docs/funding.md
+++ docs/funding.md
@@ -1,5 +1,11 @@
 # Funding
 
+
+
 We thank the The Center for Research and Education in AI and Learning (REAL@USC) for their funding and support towards this project.
 
 Any opinions, findings, and conclusions or recommendations expressed in this material are those of the author(s) and do not necessarily reflect the views of the The Center for Research and Education in AI and Learning (REAL@USC).
+
+---
+
+This material is based upon work supported by the National Science Foundation under Grant No. OAC 2138773
--- src/reproscreener/llm/prompts.py
+++ src/reproscreener/llm/prompts.py
@@ -14,8 +14,9 @@ def num_tokens_from_string(string: str, encoding_name: str) -> int:
 
 
 class OpenAILLM:
-    def __init__(self, api_key: str):
+    def __init__(self, api_key: str, engine: str = "gpt-4"):
         openai.api_key = api_key
+        self.engine = engine
 
         prompt_path = Path(__file__).parent.resolve() / "prompt.txt"
         with open(prompt_path, "r") as f:
@@ -25,13 +26,17 @@ class OpenAILLM:
         self.path = Path("case-studies/arxiv-corpus/gold_standard/abstracts")
         self.files = list(self.path.glob("*.txt"))
 
+    def read_abstract(self, file):
+        with open(file, "r") as f:
+            abstract = f.read().replace("\n", " ")
+        return abstract
+
     def analyze_abstracts(
-        self, max_papers: int = None, output_path: Path = Path("case-studies/arxiv-corpus/gold_standard/output")
+        self,
+        max_papers: int = None,
+        output_path: Path = Path("case-studies/arxiv-corpus/gold_standard/output/llm_output"),
     ) -> pd.DataFrame:
-        data = {
-            "paper": [],
-            "response_text": [],
-        }
+        output_path.mkdir(parents=True, exist_ok=True)
         files = self.files[:max_papers] if max_papers else self.files
 
         with Progress() as progress:
@@ -39,36 +44,51 @@ class OpenAILLM:
 
             for file in files:
                 log.info(f"Processing paper: {file.stem}")
-                with open(file, "r") as f:
-                    abstract = f.read().replace("\n", " ")
-                result = self._analyze_abstract(abstract)
-                data["paper"].append(file.stem)
-                data["response_text"].append(result["response_text"])
+                abstract = self.read_abstract(file)
+                result = self._analyze_abstract(abstract, file.stem)  # passing file.stem as an argument
+                df = pd.DataFrame(result, index=[0])  # convert the result to a dataframe
+                df.to_csv(output_path / f"{file.stem}_llm_results.csv", index=False)  # save the dataframe to a csv file
 
                 progress.advance(abstract_task)
 
-        df = pd.DataFrame(data)
-        df.to_csv(output_path / "llm_results.csv", index=False)
-
-        return df
-
-    def _analyze_abstract(self, abstract: str) -> dict:
+    def _analyze_abstract(self, abstract: str, file_stem: str) -> dict:  # added new argument file_stem
         log.info(f"Processing prompt")
-        prompt_tokens = num_tokens_from_string(self.prompt.format(abstract), "p50k_base")
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=self.prompt.format(abstract),
-            temperature=0.5,
-            max_tokens=4096 - len(self.prompt.format(abstract).split()) - 350,
+        prompt = self.prompt.format(abstract)
+        prompt_tokens = num_tokens_from_string(prompt, "p50k_base")
+
+        # New chat-based request
+        response = openai.ChatCompletion.create(
+            model=self.engine,
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": prompt},
+            ],
+            max_tokens=5192 - prompt_tokens - 350,
         )
         console.print(response, style="bold cyan")
         console.print(f"Tokens in prompt: {prompt_tokens}", style="bold red")
-        text = response.choices[0].text.strip()
-        return {"response_text": text}
+        text = response.choices[0].message["content"].strip()
+        return {"paper": file_stem, "response_text": text}  # use file_stem instead of file.stem
+
+    def combine_results(
+        self, output_path: Path = Path("case-studies/arxiv-corpus/gold_standard/output/llm_output")
+    ) -> pd.DataFrame:
+        dfs = []
+        for file in output_path.glob("*.csv"):
+            dfs.append(pd.read_csv(file))
+
+        combined_df = pd.concat(dfs, ignore_index=True)
+        return combined_df
+
+    def get_summary_statistics(self, df: pd.DataFrame):
+        return df.describe(include="all")
 
 
 if __name__ == "__main__":
     openai_llm = OpenAILLM(os.getenv("OPENAI_API_KEY"))
-    df = openai_llm.analyze_abstracts(max_papers=2)
-    for index, row in df.iterrows():
-        console.print(f"Paper: {row['paper']}\nResponse Text:\n{row['response_text']}\n")
+    openai_llm.analyze_abstracts(max_papers=5)
+
+    combined_df = openai_llm.combine_results()
+    print(combined_df)
+    summary = openai_llm.get_summary_statistics(combined_df)
+    print(summary)
