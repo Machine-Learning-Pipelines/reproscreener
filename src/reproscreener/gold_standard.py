a9b42e313a0aada841467cba7fc429a7359bef31
Adhithya Bhaskar <adhithyabhaskar@gmail.com>
2023-08-01 22:41:23 -0700

    gold standard heatmaps, funding, eval tables

--- src/reproscreener/gold_standard.py
+++ src/reproscreener/gold_standard.py
@@ -3,40 +3,6 @@ from reproscreener.gdrive_downloader import gdrive_get_manual_eval
 from pathlib import Path
 from typing import List
 
-
-def get_gold_standard_ids_from_manual(manual_path: Path) -> List[str]:
-    manual_df = gdrive_get_manual_eval(overwrite=False, manual_path=manual_path)
-    return manual_df["paper"].tolist()
-
-
-def summary_table(df, column, number_of_papers):
-    variable_counts = df[column].value_counts()
-    percentage = variable_counts / number_of_papers * 100
-
-    summary_table = pd.DataFrame({"Article_Count": variable_counts, "Percentage": percentage})
-    summary_table = summary_table.sort_values(by="Article_Count", ascending=False)
-    summary_table["Percentage"] = summary_table["Percentage"].map("{:.2f}%".format)
-    return summary_table
-
-
-def prepare_pivot(df, id_column, map_dict, var_column=None, match_column=None, value_column=None):
-    if var_column and match_column:
-        df["Mapped_Category"] = df[var_column].map(map_dict)
-        df.loc[df[var_column] == "Parsed Readme", "Mapped_Category"] = df.loc[
-            df[var_column] == "Parsed Readme", match_column
-        ]
-    else:
-        df["Mapped_Variable"] = df[var_column].map(map_dict)
-
-    df["Value"] = 1
-    if var_column and match_column:
-        pivot_df = df.pivot_table(values="Value", index=id_column, columns="Mapped_Category", fill_value=0)
-    else:
-        pivot_df = df.pivot_table(values="Value", index=id_column, columns="Mapped_Variable", fill_value=0)
-
-    return pivot_df
-
-
 tex_map_dict = {
     "Research questions": "research_questions",
     "Research problem": "problem",
@@ -59,23 +25,97 @@ repo_map_dict = {
 }
 
 
-def run_evaluation(df, map_dict, total_articles=50):
-    summary_df = pd.DataFrame(columns=["Category", "Article_Count", "Percentage"])
+def split_parsed_readme(df, column):
+    dummies = df[column].str.get_dummies(sep=", ")
+    df = pd.concat([df, dummies], axis=1)
+    return df
 
-    for col in map_dict.values():
-        article_count = df[col].sum() if col in df.columns else 0
-        percentage = f"{(article_count / total_articles) * 100:.2f}%"
+
+def get_gold_standard_ids_from_manual(manual_path: Path) -> List[str]:
+    manual_df = gdrive_get_manual_eval(overwrite=False, manual_path=manual_path)
+    return manual_df["paper"].tolist()
+
+
+def summary_table(df, column, number_of_papers):
+    variable_counts = df[column].value_counts()
+    percentage = variable_counts / number_of_papers * 100
+
+    summary_table = pd.DataFrame(
+        {"Reproscreener_Article_Count": variable_counts, "Reproscreener_Percentage": percentage}
+    )
+    summary_table = summary_table.sort_values(by="Reproscreener_Article_Count", ascending=False)
+    summary_table["Reproscreener_Percentage"] = summary_table["Reproscreener_Percentage"].map("{:.2f}%".format)
+    return summary_table
+
+
+def prepare_pivot(df, id_column, map_dict, var_column=None, match_column=None):
+    if var_column and match_column:
+        df["Mapped_Category"] = df[var_column].map(map_dict)
+        df.loc[df[var_column] == "Parsed Readme", "Mapped_Category"] = df.loc[
+            df[var_column] == "Parsed Readme", match_column
+        ]
+        parsed_columns = df["Mapped_Category"].str.get_dummies(sep=", ")
+        for col in parsed_columns.columns:
+            if col not in df.columns:
+                df[col] = parsed_columns[col]
+    else:
+        df["Mapped_Variable"] = df[var_column].map(map_dict)
+
+    df["Value"] = 1
+    if var_column and match_column:
+        pivot_df = df.pivot_table(values="Value", index=id_column, columns="Mapped_Category", fill_value=0)
+    else:
+        pivot_df = df.pivot_table(values="Value", index=id_column, columns="Mapped_Variable", fill_value=0)
+
+    return pivot_df
+
+
+def compare_with_manual(
+    auto_eval_df, manual_eval_df, map_dict, id_column="Paper_ID", total_articles=50, output_format="count"
+):
+    merged_df = pd.merge(auto_eval_df, manual_eval_df, on=id_column, how="outer")
+
+    results_df = pd.DataFrame(
+        columns=[
+            "Variable",
+            "False_Positives",
+            "False_Negatives",
+            "Total_Mistakes",
+            "Reproscreener_Found",
+            "Manual_Found",
+        ]
+    )
+
+    for key, col in map_dict.items():
+        reproscreener_count = merged_df[col + "_reproscreener"].sum()
+        manual_count = merged_df[col + "_manual"].sum()
+
+        false_positives = (merged_df[col + "_reproscreener"] > merged_df[col + "_manual"]).sum()
+        false_negatives = (merged_df[col + "_reproscreener"] < merged_df[col + "_manual"]).sum()
+
+        total_mistakes = false_positives + false_negatives
+
+        if output_format == "percent":
+            # convert counts to percentages
+            reproscreener_count = (reproscreener_count / total_articles) * 100
+            manual_count = (manual_count / total_articles) * 100
+            false_positives = (false_positives / total_articles) * 100
+            false_negatives = (false_negatives / total_articles) * 100
+            total_mistakes = (total_mistakes / total_articles) * 100
 
         temp_df = pd.DataFrame(
             {
-                "Category": [col],
-                "Article_Count": [article_count],
-                "Percentage": [percentage],
+                "Variable": [key],
+                "False_Positives": [false_positives],
+                "False_Negatives": [false_negatives],
+                "Total_Mistakes": [total_mistakes],
+                "Reproscreener_Found": [reproscreener_count],
+                "Manual_Found": [manual_count],
             }
         )
-        summary_df = pd.concat([summary_df, temp_df])
+        results_df = pd.concat([results_df, temp_df])
 
-    return summary_df
+    return results_df
 
 
 if __name__ == "__main__":
--- src/reproscreener/llm/spectre.py
+++ src/reproscreener/llm/spectre.py
@@ -1,18 +1,22 @@
 from pathlib import Path
+
 import pandas as pd
 import torch
+from reproscreener.utils import console
 from rich.console import Console
+from rich.table import Table
 from sklearn.metrics.pairwise import cosine_similarity
 from tqdm import tqdm
 from transformers import AutoModel, AutoTokenizer
-from reproscreener.utils import console
+from scipy.spatial.distance import cosine, euclidean, cityblock
 
 
 class AbstractEvaluator:
-    def __init__(self, model_name: str, device="cpu"):
+    def __init__(self, model_name: str, device="cpu", similarity_measure="cosine"):
         self.device = torch.device(device)
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.model = AutoModel.from_pretrained(model_name).to(self.device)
+        self.similarity_measure = similarity_measure
 
     def tokenize(self, text: str):
         return self.tokenizer.encode(text, return_tensors="pt").to(self.device)
@@ -20,12 +24,23 @@ class AbstractEvaluator:
     def embed_text(self, text: str):
         input_ids = self.tokenize(text)
         with torch.no_grad():
-            return self.model(input_ids=input_ids).pooler_output.cpu()
+            outputs = self.model(input_ids=input_ids)
+            return outputs.last_hidden_state.mean(dim=1).cpu()
 
     def get_similarity(self, em, em2):
-        return cosine_similarity(em.detach().numpy(), em2.detach().numpy())[0][0]
+        em = em.detach().numpy().reshape(-1)
+        em2 = em2.detach().numpy().reshape(-1)
+        if self.similarity_measure == "cosine":
+            return 1 - cosine(em, em2)
+        elif self.similarity_measure == "euclidean":
+            return -euclidean(em, em2)
+        elif self.similarity_measure == "manhattan":
+            return -cityblock(em, em2)
+        else:
+            raise ValueError(f"Unknown similarity measure: {self.similarity_measure}")
 
-    def evaluate_abstract(self, abstract: str, metrics: dict, threshold: float = 0.5):
+    def evaluate_abstract(self, abstract: str, metrics: dict, thresholds: dict):
+        console.print(abstract)
         results = {}
         metrics_embeddings = {metric: self.embed_text(description) for metric, description in metrics.items()}
 
@@ -33,6 +48,9 @@ class AbstractEvaluator:
             sentence_embedding = self.embed_text(sentence)
             for metric, description_embedding in metrics_embeddings.items():
                 score = self.get_similarity(sentence_embedding, description_embedding)
+                threshold = thresholds.get(
+                    metric, 0.5
+                )  # Use a default threshold of 0.5 if none is provided for this metric
                 if score > threshold and (metric not in results or score > results[metric]):
                     results[metric] = score
         return results
@@ -52,8 +70,13 @@ def process_abstracts(
     output_folder: Path,
     console: Console,
     metrics: dict,
+    limit: int = None,
 ):
     abstract_paths = list(abstract_folder.glob("*.txt"))
+
+    # Limit the number of papers if limit is set
+    if limit is not None:
+        abstract_paths = abstract_paths[:limit]
 
     all_results = []
 
@@ -70,7 +93,33 @@ def process_abstracts(
 
     df = pd.DataFrame(all_results)
     df.set_index("Abstract file", inplace=True)
-    console.print(df.to_markdown())
+
+    # Create a new table
+    table = Table(show_header=True, header_style="bold magenta")
+
+    # Add a column for paper id's
+    table.add_column("Paper ID")
+
+    # Add columns for each metric
+    for column in df.columns:
+        table.add_column(column)
+
+    # Add rows
+    for index, row in df.iterrows():
+        table.add_row(index, *[f"{value:.3f}" for value in row.values])
+
+    console.print(table)
+
+
+def test_abstract(evaluator: AbstractEvaluator, abstract: str, metrics: dict, thresholds: dict):
+    results = evaluator.evaluate_abstract(abstract, metrics, thresholds)
+    console = Console()
+    table = Table(show_header=True, header_style="bold magenta")
+    table.add_column("Metric")
+    table.add_column("Score")
+    for metric, score in results.items():
+        table.add_row(metric, f"{score:.3f}")
+    console.print(table)
 
 
 if __name__ == "__main__":
@@ -87,6 +136,7 @@ if __name__ == "__main__":
     #     "Software dependencies": "Software packages/dependencies used to run the code",
     #     "Experiment setup": "Is the experimental setup described?",
     # }
+
     metrics = {
         "Problem": "research problem, problem, problem statement",
         "Objective": "goal of the paper, objective of the paper, aim of the paper",
@@ -101,12 +151,63 @@ if __name__ == "__main__":
         "Experiment setup": "experimental setup, hyperparameters, initial values, selected models",
     }
 
+    cosine_thresholds = {
+        "Problem": 0.75,
+        "Objective": 0.75,
+        "Research method": 0.75,
+        "Research questions": 0.75,
+        "Pseudocode": 0.75,
+        "Dataset": 0.75,
+        "Hypothesis": 0.75,
+        "Prediction": 0.75,
+        "Method source code": 0.75,
+        "Software dependencies": 0.75,
+        "Experiment setup": 0.75,
+    }
+
+    euclidean_thresholds = {
+        "Problem": -0.7,
+        "Objective": -0.7,
+        "Research method": -0.7,
+        "Research questions": -0.7,
+        "Pseudocode": -0.7,
+        "Dataset": -0.7,
+        "Hypothesis": -0.7,
+        "Prediction": -0.7,
+        "Method source code": -0.7,
+        "Software dependencies": -0.7,
+        "Experiment setup": -0.7,
+    }
+
+    manhattan_thresholds = {
+        "Problem": -0.7,
+        "Objective": -0.7,
+        "Research method": -0.7,
+        "Research questions": -0.7,
+        "Pseudocode": -0.7,
+        "Dataset": -0.7,
+        "Hypothesis": -0.7,
+        "Prediction": -0.7,
+        "Method source code": -0.7,
+        "Software dependencies": -0.7,
+        "Experiment setup": -0.7,
+    }
+
     device = "cuda" if torch.cuda.is_available() else "cpu"
     console.print(f"Using device: {device}\n")
-    evaluator = AbstractEvaluator("allenai/specter2", device=device)
+    # evaluator = AbstractEvaluator("allenai/specter2", device=device)
+    evaluator1 = AbstractEvaluator("allenai/specter2", device=device, similarity_measure="cosine")
+    evaluator2 = AbstractEvaluator("allenai/specter2", device=device, similarity_measure="euclidean")
+    evaluator3 = AbstractEvaluator("allenai/specter2", device=device, similarity_measure="manhattan")
 
     abstract_folder_path = Path("case-studies/arxiv-corpus/gold_standard/abstracts/")
     output_folder = Path("outputs")
     output_folder.mkdir(parents=True, exist_ok=True)
 
-    process_abstracts(evaluator, abstract_folder_path, output_folder, console, metrics)
+    # process_abstracts(evaluator, abstract_folder_path, output_folder, console, metrics)
+    abstract = "Deep Learning (DL) methods have emerged as one of the most powerful tools for functional approximation and prediction. While the representation properties of DL have been well studied, uncertainty quantification remains challenging and largely unexplored. Data augmentation techniques are a natural approach to provide uncertainty quantification and to incorporate stochastic Monte Carlo search into stochastic gradient descent (SGD) methods. The purpose of our paper is to show that training DL architectures with data augmentation leads to efficiency gains. We use the theory of scale mixtures of normals to derive data augmentation strategies for deep learning. This allows variants of the expectation-maximization and MCMC algorithms to be brought to bear on these high dimensional nonline"
+    abstract = "hello my name is adhithya. problem."
+
+    test_abstract(evaluator1, abstract, metrics, cosine_thresholds)
+    test_abstract(evaluator2, abstract, metrics, euclidean_thresholds)
+    test_abstract(evaluator3, abstract, metrics, manhattan_thresholds)
