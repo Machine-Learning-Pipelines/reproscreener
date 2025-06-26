d532dd619e1910fcdd13304dd4d6504e8358ae78
Adhithya Bhaskar <adbX@users.noreply.github.com>
2025-06-26 11:04:59 -0700

    add quartodoc

--- src/reproscreener/main.py
+++ src/reproscreener/main.py
@@ -3,8 +3,8 @@ from .repo_analyzer import main as repo_analyzer_main
 from .manual_evaluations.manual_eval import main as manual_eval_main
 
 def main():
-    # paper_analyzer_main()
-    # repo_analyzer_main()
+    paper_analyzer_main()
+    repo_analyzer_main()
     manual_eval_main()
 
 if __name__ == "__main__":
